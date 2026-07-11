b306fcbe8e11cdab19ba2e59a51618203a7f966f
elytrapro2000 <224826062+elytrapro2000@users.noreply.github.com>
2026-07-11 13:32:06 +0200

    Updated Version Number

--- SD_FOV_Settings_Mod/AssemblyInfo.cs
+++ SD_FOV_Settings_Mod/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using MelonLoader;
 using SD_FOV_Settings_Mod;
 
-[assembly: MelonInfo(typeof(FOV_Mod), "FOV Mod", "1.0.0", "elytrapro2000")]
+[assembly: MelonInfo(typeof(FOV_Mod), "FOV Mod", "1.0.1", "elytrapro2000")]
 [assembly: MelonGame("CuriousOwlGames", "StellarDrive")]
